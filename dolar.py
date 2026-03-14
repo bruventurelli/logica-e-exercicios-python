1a0d2142065079c14a4c6b9a18c080e4061d0f64
bruventurelli <brunovfv@gmail.com>
2026-03-14 02:36:04 -0300

    Add currency conversion from reais to dollars

---
+++ dolar.py
@@ -0,0 +1,5 @@
+reais = float(input("quanto dinheiro em reais você possui?"))
+dolar = 5.33
+quantidade = reais / dolar 
+print(f"Você pode comprar a seguinte quantidade de dólares de acordo com a cotação atual: ")
+print(f".US${quantidade:.2f}")
