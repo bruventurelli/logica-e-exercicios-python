abfed5929efe2f8aa836e496d2f372beba75f92b
bruventurelli <brunovfv@gmail.com>
2026-03-14 02:33:18 -0300

    media.py

---
+++ media.py
@@ -0,0 +1,4 @@
+nota1 = float(input("Resultado da primeira prova: "))
+nota2 = float(input("Resultado da segunda prova: "))
+media = (nota1 + nota2) / 2
+print(f"A sua média na disciplina foi de: {media}")
