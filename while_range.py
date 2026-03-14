7f002c9131eef88ec2e7f7e82e558f6025e6deb9
bruno vasconcelos ferrarini venturelli <brunovfv@gmail.com>
2026-03-14 04:09:32 -0300

    Implement range and while loops for multiplication
    Fix while loop to increment the counter correctly.

---
+++ while_range.py
@@ -0,0 +1,9 @@
+print("Números de 1 a 6 multiplicados por 2: ")
+for numero in range (1,7):
+    print(numero*2)
+
+print("números de 1 a 5 multiplicados por 2 whilte:")
+contador = 1
+while contador <=5:
+    print(contador*2)
+    contador+=1
